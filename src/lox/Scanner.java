53cab701b8fa0671eba11dc37c7a023980df52e8
Jakob Laenge <jakob.laenge@hotmail.com>
2024-04-06 08:00:41 +0900

    Intern. Adds one and two character tokens and string and number literals.

--- src/lox/Scanner.java
+++ src/lox/Scanner.java
@@ -35,8 +35,136 @@ public class Scanner {
 		return (current >= source.length());
 	}
 	
-	private void scanToken() {}
+	private void scanToken() {
+		char c = advance();
+		switch(c) {
+		
+			// Single-character tokens.
+			case '(': addToken(LEFT_PARENTHESIS); break;
+			case ')': addToken(RIGHT_PARENTHESIS); break;
+			case '{': addToken(LEFT_BRACE); break;
+			case '}': addToken(RIGHT_BRACE); break;
+			case ',': addToken(COMMA); break;
+			case '.': addToken(DOT); break;
+			case '-': addToken(MINUS); break;
+			case '+': addToken(PLUS); break;
+			case ';': addToken(SEMICOLON); break;
+			case '*': addToken(STAR); break;
+			
+			// One or two character tokens.
+			case '!':
+				if(match('=')) addToken(BANG_EQUAL);
+				else addToken(BANG);
+				break;
+			case '=':
+				if(match('=')) addToken(EQUAL_EQUAL);
+				else addToken(EQUAL);
+				break;
+			case '<':
+				if(match('=')) addToken(LESS_EQUAL);
+				else addToken(LESS);
+				break;
+			case '>':
+				if(match('=')) addToken(GREATER_EQUAL);
+				else addToken(GREATER);
+				break;
+				
+			// Comments.
+			case '/':
+				if(match('/')) {
+					while(!isAtEnd() && peek() != '\n') advance();
+				} else {
+					addToken(SLASH);
+				}
+				break;
+				
+			// Whitespaces.
+			case ' ':
+			case '\r':
+			case '\t':
+				break;
+				
+			case '\n':
+				line++;
+				break;
+				
+			// String Literals.
+			case '"': string(); break;
+			
+			// Literals, Keywords and Erros.
+			default:
+				
+				// numbers
+				if(isDigit(c)) number();
+				
+				// errors
+				else Lox.error(line, "Unexpected character: '" + c + "'.");
+				break;
+			
+		}
+	}
 	
+	private boolean isDigit(char c) {
+		return '0' <= c && c <= '9';
+	}
 	
+	private void string() {
+		while(!isAtEnd() && peek() != '"') {
+			if(peek() == '\n') line++;
+			advance();
+		}
+		
+		if(isAtEnd()) {
+			Lox.error(line, "Unterminated string.");
+			return;
+		}
+		
+		advance();
+		String value = source.substring(start+1, current-1);
+		addToken(STRING, value);
+	}
+	
+	private void number() {
+		while(isDigit(peek())) advance();
+		
+		if(peek() == '.' && isDigit(peekNext())) {
+			advance();
+			while(isDigit(peek())) advance();
+		}
+		
+		addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+	}
+	
+	private char advance() {
+		assert(current < source.length());
+		current++;
+		return source.charAt(current - 1);
+	}
+	
+	private void addToken(TokenType type) {
+		addToken(type, null);
+	}
+	
+	private void addToken(TokenType type, Object literal) {
+		String text = source.substring(start, current);
+		tokens.add(new Token(type, text, literal, line));
+	}
+	
+	private boolean match(char expected) {
+		if(isAtEnd()) return false;
+		if(source.charAt(current) != expected) return false;
+		
+		current++;
+		return true;
+	}
+	
+	private char peek() {
+		if(isAtEnd()) return '\0';
+		return source.charAt(current);
+	}
+	private char peekNext() {
+		if(current+1 >= source.length()) return '\0';
+		return source.charAt(current+1);
+	}
 
 }
--- src/lox/TokenType.java
+++ src/lox/TokenType.java
@@ -4,7 +4,7 @@ public enum TokenType {
 
 	// Single-character tokens.
 	LEFT_PARENTHESIS, RIGHT_PARENTHESIS, LEFT_BRACE, RIGHT_BRACE,
-	COMMA, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+	COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
 	
 	// One or two character tokens.
 	BANG, BANG_EQUAL,

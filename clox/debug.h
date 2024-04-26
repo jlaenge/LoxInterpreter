b36644cf3f0822cd1b0de676578668399ce8ec58
Jakob Laenge <jakob.laenge@hotmail.com>
2024-04-26 18:52:14 +0900

    Intern. Adds disassembler for chunk.

---
+++ clox/debug.h
@@ -0,0 +1,10 @@
+#ifndef CLOX_DEBUG_H_
+#define CLOX_DEBUG_H_
+
+#include "common.h"
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif

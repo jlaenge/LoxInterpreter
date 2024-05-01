7d5ea84fa4f4ad50e60da3735d5017dec265c54b
Jakob Laenge <jakob.laenge@hotmail.com>
2024-05-01 10:04:04 +0800

    Intern. Adds scanner for one and two character tokens.

--- clox/compiler/scanner.c
+++ clox/compiler/scanner.c
@@ -28,7 +28,6 @@ static Token makeToken(TokenType type) {
 	token.start = scanner.start;
 	token.length = (int)(scanner.current - scanner.start);
 	token.line = scanner.line;
-
 	return token;
 }
 static Token errorToken(const char* message) {
@@ -40,11 +39,83 @@ static Token errorToken(const char* message) {
 	token.line = scanner.line;
 	return token;
 }
+static char advance() {
+	char current = *scanner.current;
+	scanner.current++;
+	return current;
+}
+static char peek() {
+	return *scanner.current;
+}
+static char peekNext() {
+	if(isAtEnd()) return '\0';
+	else return scanner.current[1];
+}
+static bool match(char expected) {
+	if(isAtEnd()) return false;
+	if(*scanner.current != expected) return false;
+	scanner.current++;
+	return true;
+}
+static void skipWhitespaces() {
+	while(true) {
+		char c = peek();
+		switch(c) {
+
+			// whitespaces
+			case ' ':
+			case '\r':
+			case '\t':
+				advance();
+				break;
+
+			// comments
+			case '/':
+				if(peekNext() == '/') {
+					while(!isAtEnd() && peek() != '\n') advance();
+				} else {
+					return;
+				}
+				break;
+
+			// newlines
+
+			default:
+				return;
+		}
+	}
+}
 
 Token scanToken() {
+	skipWhitespaces();
+
 	scanner.start = scanner.current;
 
 	if(isAtEnd()) return makeToken(EOF);
+
+	char c = advance();
+	switch(c) {
+
+		// single-character tokens
+		case '(': return makeToken(TOKEN_LEFT_PARENTHESIS);
+		case ')': return makeToken(TOKEN_RIGHT_PARENTHESIS);
+		case '{': return makeToken(TOKEN_LEFT_BRACE);
+		case '}': return makeToken(TOKEN_RIGHT_BRACE);
+		case ',': return makeToken(TOKEN_COMMA);
+		case '.': return makeToken(TOKEN_DOT);
+		case ';': return makeToken(TOKEN_SEMICOLON);
+		case '+': return makeToken(TOKEN_PLUS);
+		case '-': return makeToken(TOKEN_MINUS);
+		case '*': return makeToken(TOKEN_STAR);
+		case '/': return makeToken(TOKEN_SLASH);
+
+		// one or two character tokens
+		case '!': return makeToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+		case '=': return makeToken(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+		case '<': return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+		case '>': return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+
+	}
 
 	return errorToken("Unexpected character.");
 }
